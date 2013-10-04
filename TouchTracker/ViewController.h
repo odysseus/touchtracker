f59b377b07359fba975f94a8a157cdb691f8f2db
Ryan Case <rmcase@gmail.com>
2013-10-04 15:29:01 -0600

    Initial Commit

---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by Ryan Case on 10/4/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
